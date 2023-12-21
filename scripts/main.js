b52e750cd0a5e6a7dc1b08bbefa9c028739223e9
John Lynch <johnlynch.32768@gmail.com>
2023-12-21 04:54:17 +0000

    Adjust styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+let notes = [];
+
 class NoteBox extends HTMLElement {
     static z_index = 0;
     constructor(title, items) {
@@ -22,6 +24,7 @@ class NoteBox extends HTMLElement {
         this.addEventListener('mousedown', this.handle_mousedown.bind(this));
         document.addEventListener('mousemove', this.handle_mousemove.bind(this));
         document.addEventListener('mouseup', this.handle_mouseup.bind(this));
+        this.style.transition = 'opacity 3s ease-in-out';
     }
 
     handle_mousedown(e) {
@@ -67,11 +70,12 @@ class NoteBox extends HTMLElement {
 customElements.define('note-box', NoteBox);
 
 // For debugging, create a load of notes in random positions
+const workspace = document.getElementById('workspace');
+const workspace_rect = workspace.getBoundingClientRect();
 for (title of titles) {
-    const workspace = document.getElementById('workspace');
-    const workspace_rect = workspace.getBoundingClientRect();
     const item_list = Array(rand_in_range(2, 11)).fill(0).map(item => entries[rand_int(item_count)]);
     const note = new NoteBox(title, item_list);
+    notes.push(note);
     note.style.backgroundColor = colours[rand_int(colour_count)];
     workspace.appendChild(note);
     const note_rect = note.getBoundingClientRect();
@@ -81,12 +85,15 @@ for (title of titles) {
     const y1 = y0 + workspace_rect.height - note_rect.height - 8;
     note.style.left = `${rand_in_range(x0, x1)}px`;
     note.style.top = `${rand_in_range(y0, y1)}px`;
-    console.log(`Note ${title}: top left: (${note.style.left}, ${note.style.top}), width: ${note_rect.width}, height: ${note_rect.height}`);
+    // console.log(`Note ${title}: top left: (${note.style.left}, ${note.style.top}), width: ${note_rect.width}, height: ${note_rect.height}`);
 }
 
 const new_note_button = document.getElementById('new-note');
 new_note_button.addEventListener('click', event => {
-    console.log(`${event.target} clicked.`);
+    // console.log(`${event.target} clicked.`);
+    for (const note of notes) {
+        note.style.opacity = 0.2;
+    }
     const template = document.getElementById('note-box-form');
     const div = document.createElement('div');
     div.appendChild(template.content.cloneNode(true));
@@ -97,23 +104,31 @@ new_note_button.addEventListener('click', event => {
     const create_button = div.querySelector('#create-note');
     [create_button, cancel_button].forEach(button => {
         button.addEventListener('click', event => {
-            console.log(event.target.id);
+            // console.log(event.target.id);
             switch(event.target.id) {
                 case 'cancel':
                     div.remove();
                     break;
-                    case 'create-note':
+                case 'create-note':
                         const data = new FormData(form);
                         const title = data.get('title');
                         const items = Array(6).fill(1).map((el, i) => data.get(`item_${i + 1}`)).filter(x => x);
                         div.remove();
                         console.log(title, items);
                         const note = new NoteBox(title, items);
+                        notes.push(note);
                         note.style.backgroundColor = colours[rand_int(colour_count)];
                         workspace.appendChild(note);
-                        note.style.left = `500px`;
-                        note.style.top = `400px`;
+                        const workspace_rect = note.parentElement.getBoundingClientRect();
+                        const note_rect = note.getBoundingClientRect();
+                        const left = workspace_rect.left + 0.5 * (workspace_rect.width - note_rect.width);
+                        const top = workspace_rect.top + 0.5 * (workspace_rect.height - note_rect.height); 
+                        note.style.left = `${left}px`;
+                        note.style.top = `${top}px`;
                         note.style.zIndex = ++NoteBox.z_index;
+                        for (const note of notes) {
+                            note.style.opacity = 1.0;
+                        }
                         break;
                 default:
                     console.log(`Something else clicked - event targeted was ${event.target}.`);
